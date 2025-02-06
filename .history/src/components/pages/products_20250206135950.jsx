302087aaa881d913edce1309eccd9e17f26987a7
Akainu <akainu@localhost>
2025-02-06 15:09:57 +0100

    mouff

---
+++ .history/src/components/pages/products_20250206135950.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+const Products = ({
+  api,
+  loading,
+  images,
+  filteredProducts,
+  setSelectedProduct,
+  addToCart,
+  handleShowAlert,
+  showAlert,
+  searchTerm,
+  highlightText,
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const navigate = useNavigate();
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+    navigate("/selectedProduct");
+  };
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  return (
+    <>
+      <div style={{ marginTop: "20px", display: "flex", flexWrap: "wrap" }}>
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
+            <div
+              key={product.id}
+              style={{
+                border: "1px solid #ddd",
+                padding: "0px",
+                borderRadius: "8px",
+                maxWidth: "300px",
+                maxHeight: "600px",
+                margin: "15px",
+              }}
+            >
+              <div>
+                {product.images.length > 0 ? (
+                  <img
+                    src={product.images[0]} // Display first image only
+                    alt={product.name}
+                    style={{
+                      width: "300px",
+                      height: "300px",
+                      objectFit: "cover",
+                      borderRadius: "8px",
+                    }}
+                  />
+                ) : (
+                  <p>No image available</p>
+                )}
+              </div>
+              <span>
+                <h5 className="name">
+                  <span
+                    style={{ color: "black" }}
+                    dangerouslySetInnerHTML={{
+                      __html: highlightText(product.name, searchTerm),
+                    }}
+                  ></span>{" "}
+                </h5>
+                <h5>Description: {product.description}</h5>
+                <h5>Price: {product.price}</h5>
+                <div>
+                  <button>addToCart</button>
+                  <button> +WishList</button>
+                </div>
+              </span>
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Products;
---
+++ .history/src/components/pages/products_20250206140600.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import "./products.css"
+import { useNavigate } from 'react-router-dom';
+
+
+const Products = ({api,loading,images, filteredProducts, setSelectedProduct , addToCart, handleShowAlert ,showAlert, searchTerm, highlightText}) => {
+ const [currentIndex, setCurrentIndex] = useState(0)
+
+  
+  const navigate = useNavigate();
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+    navigate("/selectedProduct")
+  };
+  const nextImage =() =>{
+    setCurrentIndex((prevIndex) => (prevIndex +1) %images.length) 
+  }
+
+  return (
+    <>
+  
+
+    <div className='box1'>
+            <h1></h1>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}   >
+                  
+                    {/* <div>
+                        <a className='btn btn-primary btn-sm me-1' href="">
+                            Edit
+                        </a>
+                        <button type='button' className='btn btn-danger btn-sm'>Delete</button>
+                    </div> */}
+                  {filteredProducts.map((product, index) => (
+                   
+                    <div key={product.id} className='box2'  >
+                      {product.images.map((image, index) =>(
+
+                    
+                    <img className='image'
+                        key={index}
+                      src={`${api}$image [0]}
+                        alt={product.name}
+                        onClick={() => handleProductClick(product)}
+                       
+                        />
+                      ))}
+                         <span className='text'>
+                            <a  className='name'><span style={{color:"black"}} 
+                                dangerouslySetInnerHTML={{
+                                __html: highlightText(product.name, searchTerm),
+                                       }}
+                                 ></span> </a>
+                            <a>Description: {product.desc}</a>
+                            <a>Price: {product.price}</a>
+                         </span>
+                         <div className='cart'>
+                            <button className='btn btn-warning' onClick={ () => addToCart(product)} >Add to Cart</button>
+                            
+                         </div>
+                          
+                         
+                    </div>
+
+                  ))}
+                  
+                  <button onClick={handleShowAlert}>Show Custom Alert</button>
+      {showAlert && (
+        <div style={{ padding: "10px", backgroundColor: "red", color: "white" }}>
+          This is a custom alert!
+        </div>
+      )}
+      
+
+            
+     
+                </div>
+               
+            )}
+            
+        </div > 
+            </>
+  )
+}
+
+export default Products
