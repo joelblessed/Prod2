7eef69d3ebdd1c921b90f7133250c277cfbd4d68
Akainu <akainu@localhost>
2025-02-11 18:19:23 +0100

    api address

---
+++ .history/src/components/pages/Home_20250211110442.jsx
@@ -0,0 +1,151 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import "./products.css";
+
+// import { createContext,useState,useContext } from 'react';
+
+const Home = ({
+  api,
+  recieveName,
+  filteredProducts,
+  setSelectedProduct,
+  addToCart,
+  addToWishList,
+  inwishlist,
+  handleShowAlert,
+  showAlert,
+  searchTerm,
+  highlightText,
+}) => {
+const [product, setProducts] = useState(null);
+
+ 
+  const navigate = useNavigate();
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+    navigate("/selectedProduct");
+  };
+
+   // Toggle the selection state of a product
+   const toggleSelection = (id, currentSelection) => {
+    // Update the product on the server using PATCH
+    fetch(`${api}/updateProducts/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ isSelected: !currentSelection }),
+    })
+      .then((response) => response.json())
+      .then((updatedProduct) => {
+        // Update the local state to reflect the change
+        setProducts((prevProducts) =>
+          prevProducts.map((product) =>
+            product.id === id ? updatedProduct : product
+          )
+        );
+      })
+      .catch((error) => console.error("Error updating product:", error));
+  };
+
+  return (
+    <>
+      <div
+        className=""
+        style={{ marginTop: "20px", display: "flex", flexWrap: "wrap" }}
+      >
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
+            <div
+              key={product.id}
+              style={{
+                border: "1px solid red",
+                borderRadius: "10px",
+                maxWidth: "400px",
+                maxHeight: "600px",
+                margin: "15px",
+              }}
+            >
+              {product.images.length > 0 ? (
+                <img
+                  src={product.images[0]} // Display first image only
+                  alt={product.name}
+                  style={{
+                    width: "300px",
+                    height: "300px",
+                    objectFit: "cover",
+                    borderRadius: "10px",
+                  }}
+                  onClick={() => handleProductClick(product)}
+
+                 
+                />
+              ) : (
+                <p>No image available</p>
+              )}
+              <span
+                className="text"
+                style={{
+                  background:
+                    " linear-gradient(-45deg,white 33%, #feb47b 33%, #86a8e7 66%, #91eae4 66%)",
+                    borderRadius:"10PX"
+                }}
+              >
+                <h5 className="name">
+                  <span
+                    style={{ color: "black" }}
+                    dangerouslySetInnerHTML={{
+                      __html: highlightText(product.name, searchTerm),
+                    }}
+                  ></span>{" "}
+                </h5>
+                <h5>Description: {product.desc}{product.id}</h5>
+                <h2>available:{inwishlist ? "yes": "no"}</h2>
+                <h5>
+                  Price: {product.price}{inwishlist.name}{" "}
+                  <button  style={{
+                      background: "transparent",
+                      border: "1px solid orange",
+                      color: "orange",
+                      padding: "0px 20px",
+                      borderRadius: "10px 10px 10px 10px ",
+                      cursor: "pointer",
+                      fontSize: "40px",
+                      fontWeight:"bold",
+                      transition: "all 0.3s ease",
+                      marginLeft:"140px"
+                      
+                    }} onClick={() => addToWishList(product)}>+</button>
+                </h5>
+                <div style={{ background: "" }}>
+                  <button
+                    style={{
+                      background: "white",
+                      border: "1px solid RED",
+                      color: "red",
+                      padding: "10px 20px",
+                      borderRadius: "0px 0px 10px 10px ",
+                      cursor: "pointer",
+                      fontSize: "100%",
+                      transition: "all 0.3s ease",
+                      width: "100%",
+                    }}
+                    onClick={() => addToCart(product)}
+                  >
+                    addToCart
+                  </button>
+                </div>
+              </span>
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+      {/* {product.isSelected ? "Unselect" : "Select"} */}
+    </>
+            
+  );
+};
+
+export default Home;
