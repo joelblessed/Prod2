46b8a3bc2f5d332169af221604a63909f42b8481
joelblessed <joelblessedgod@gmail.com>
2025-04-05 20:11:37 +0000

    searches2

--- .history/src/config_20250405200913.js
+++ .history/src/config_20250405200913.js
@@ -3,5 +3,5 @@ export const api = "https://prod2-api.onrender.com";
 // "http://localhost:5000",
 // https://prod2-api.onrender.com
 
-// "typescript": "^5.8.2",
-// "typescript": "^4.9.5",
+"typescript": "^5.8.2",
+"typescript": "^4.9.5",
