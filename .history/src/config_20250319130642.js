8753578a27dad76ed4b6f2e664a3c3945538bf9a
joelblessed <joelblessedgod@gmail.com>
2025-04-18 17:58:27 +0000

    logs

--- .history/src/config_20250319130642.js
+++
@@ -1,4 +0,0 @@
-// "http://localhost:5000",
-export const api =  "https://prod2-api.onrender.com" || ;
-// "http://localhost:5000",
-// https://prod2-api.onrender.com
--- .history/src/config_20250319130737.js
+++
@@ -1,4 +0,0 @@
-
-export const api =  ["https://prod2-api.onrender.com"];
-// "http://localhost:5000",
-// https://prod2-api.onrender.com
